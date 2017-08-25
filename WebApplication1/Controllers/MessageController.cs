fffde493e439a9d5b7872d42b38ac77ca8075f1e
qo4rmp1 <qo4rmp1@gmail.com>
2017-08-25 14:27:04 +0800

    完成小型留言板

---
+++ WebApplication1/Controllers/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class MessageController : Controller
+    {
+        // 實作Service物件
+        messageDBService data = new messageDBService();
+        // GET: Message
+        public ActionResult Index()
+        {
+            return View(data.GetData());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string title, string content)
+        {
+            data.DBCreate(title, content);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication1/Services/messageDBService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class messageDBService
+    {
+        public messageEntities db = new messageEntities();
+
+        // 取得資料庫中Article資料
+        public List<Article> GetData()
+        {
+            return db.Article.ToList();
+        }
+        
+        public void DBCreate(string title, string content)
+        {
+            Article article = new Article
+            {
+                Title = title,
+                Content = content,
+                Time = DateTime.Now
+            };
+
+            db.Article.Add(article);
+            db.SaveChanges();
+        }
+    }
+}
