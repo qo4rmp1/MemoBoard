0c8ac08c83132ab027f80b67aab5848c1a89c971
qo4rmp1 <qo4rmp1@gmail.com>
2017-09-08 14:15:03 +0800

    1. 加入Hello World測試 2. 加入ELMAH.MVC第三方元件

---
+++ WebApplication1.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApplication1.Tests
+{
+    [TestClass]
+    public class FirstUnitTest
+    {
+        [TestMethod]
+        public void HelloWorld()
+        {
+            var actual = "Hello World";
+            Assert.AreEqual("Hello World", actual);
+        }
+        
+    }
+}
